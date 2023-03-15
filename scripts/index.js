2acff425d5ec81d922004397336eca602a7fdf8e
Fredrick97-coder <119665220+FredrickOtabil@users.noreply.github.com>
2023-03-15 09:32:16 +0000

    Added the close button with its functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,26 @@
 const btn = document.getElementById('hamburger');
 const links = document.querySelectorAll('.nav-link');
+const closeMenu = document.getElementById('close-menu');
 
-function ShowMenuItems() {
+function ShowMenuItems(data) {
   const menu = document.querySelector('.list');
-  menu.classList.toggle('show');
+  if (data === 'btn') {
+    menu.classList.toggle('show');
+    btn.classList.add('hide');
+    closeMenu.classList.add('show');
+  } else if (data === 'link') {
+    menu.classList.toggle('show');
+    btn.classList.remove('hide');
+    closeMenu.classList.remove('show');
+  } else {
+    menu.classList.remove('show');
+    btn.classList.remove('hide');
+    closeMenu.classList.remove('show');
+  }
 }
 
-btn.addEventListener('click', ShowMenuItems);
+btn.addEventListener('click', () => ShowMenuItems('btn'));
 links.forEach((link) => {
-  link.addEventListener('click', ShowMenuItems);
+  link.addEventListener('click', () => ShowMenuItems('link'));
 });
+closeMenu.addEventListener('click', ShowMenuItems);
